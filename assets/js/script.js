eea88a42d6cce231ad76f441112e248fbda14ca8
Wilson <waldmendes@live.com>
2025-04-10 16:44:55 +0100

    Set up structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function rungame() {
+
+}
+
+function checkanswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}  
+
+function displayAdditionQuestion() {
+
+}   
+
+function subtractQuestion() {
+
+}   
+
+function multiplyQuestion() {
+
+}
+
+function divisionQuestion() {
+    
+}
