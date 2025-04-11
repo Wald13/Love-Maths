8d8ed96fe421f34921cfa230f73a8f25b1082707
Wilson <waldmendes@live.com>
2025-04-11 14:46:45 +0100

    Add division game type

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -86,6 +88,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if(operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -138,6 +142,10 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 
-function divisionQuestion() {
-
+function displayDivisionQuestion(operand1, operand2) {
+    let divided = operand1 * operand2;
+    let divisor = operand1 
+    document.getElementById('operand1').textContent = divided;
+    document.getElementById('operand2').textContent = divisor;
+    document.getElementById('operator').textContent = "/";
 }
